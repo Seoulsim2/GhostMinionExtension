aa00cf677cf8e08a76fbd5cb81f0397932a246ad
m8simmon <magistras42@gmail.com>
2026-03-24 11:43:36 -0400

    updated scripts and added old spec results

---
+++ scripts/build_spec06_updated.sh
@@ -0,0 +1,17 @@
+cd ..
+BASE=$(pwd)
+mkdir specmnt
+mkdir SPEC
+sudo mount -o loop cpu2006*.iso specmnt
+cd specmnt
+./install.sh -d ../SPEC
+cd ..
+sudo umount specmnt
+rm -r specmnt
+cp spec_confs/aarch64.cfg SPEC/config
+cd SPEC
+. ./shrc
+# removed gamess, mcf - missing from SPEC2006 (v1.2) image  
+runspec --config=aarch64.cfg --action=build astar bwaves bzip2 cactusADM calculix gcc GemsFDTD gobmk gromacs h264ref hmmer lbm leslie3d libquantum milc namd omnetpp povray sjeng soplex tonto xalancbmk zeusmp -I
+runspec --config=aarch64.cfg --action=run --size=ref astar bwaves bzip2 cactusADM calculix gcc GemsFDTD gobmk gromacs h264ref hmmer lbm leslie3d libquantum milc namd omnetpp povray sjeng soplex tonto xalancbmk zeusmp --noreportable --iterations=1  -I
+cd $BASE/scripts
